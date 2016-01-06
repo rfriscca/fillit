1aecf67b4e965dc128f4c3274d852f0262493bd6
Remi FRISCCA <rfriscca@e3r10p4.42.fr>
2016-01-06 15:04:39 +0100

    new fix

--- srcs/valid_piece.c
+++ srcs/valid_piece.c
@@ -6,35 +6,44 @@
 /*   By: rfriscca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 13:09:31 by rfriscca          #+#    #+#             */
-/*   Updated: 2016/01/06 13:22:57 by rfriscca         ###   ########.fr       */
+/*   Updated: 2016/01/06 15:03:16 by rfriscca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		valid(char *str, int j, int i, int x)
+{
+	if (str[i + 1] == '#' || str[i + 5] == '#' || ((str[i + 4] == '#' ||
+					str[i + 3] == '#') && str[i - 1] == '#'))
+		++j;
+	else if (j == 3 * (x + 1) + x && (str[i - 1] == '#' ||
+				str[i - 5] == '#'))
+		++j;
+	return (j);
+}
 
 int		valid_piece(char *str, int n)
 {
 	int		i;
 	int		j;
 	int		x;
+	int		k;
 
 	i = 0;
 	j = 0;
+	k = 0;
 	x = 0;
 	while (str[i] == '.' || str[i] == '#' || str[i] == '\n')
 	{
 		if (str[i] == '#')
 		{
-			if (str[i + 1] == '#' || str[i + 5] == '#' || ((str[i + 4] == '#' ||
-							str[i + 3] == '#') && str[i - 1] == '#'))
-				++j;
-			else if (j == 3 * (x + 1) + x && (str[i - 1] == '#' ||
-						str[i - 5] == '#'))
-				++j;
+			++k;
+			j = valid(str, j, i, x);
 		}
 		if (str[i] == '\n' && str[i - 1] == '\n')
 			++x;
 		++i;
 	}
-	if (j != (4 * n) || i != (20 * n + 1 * (n - 1)))
+	if (j != (4 * n) || k != (4 * n) || i != (20 * n + 1 * (n - 1)))
 		return (0);
 	return (1);
 }
